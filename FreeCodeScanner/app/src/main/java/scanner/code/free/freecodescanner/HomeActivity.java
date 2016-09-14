682ab7f85d1540a31f76956097554508ff5db047
parmesh <parmesh.kumar.mahore1@huawei.com>
2016-09-14 23:01:20 +0530

    created home screen

--- FreeCodeScanner/app/src/main/java/scanner/code/free/freecodescanner/HomeActivity.java
+++ FreeCodeScanner/app/src/main/java/scanner/code/free/freecodescanner/HomeActivity.java
@@ -8,11 +8,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
-public class HomeActivity extends AppCompatActivity {
+
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     private AdView mAdView;
 
@@ -27,6 +30,13 @@ public class HomeActivity extends AppCompatActivity {
         mAdView = (AdView) findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
+
+
+        ImageButton qrScannerImageView = (ImageButton) findViewById(R.id.qr_code_image_button);
+        ImageButton barCodeScanner = (ImageButton) findViewById(R.id.bar_code_image_button);
+
+        qrScannerImageView.setOnClickListener(this);
+        barCodeScanner.setOnClickListener(this);
     }
 
     @Override
@@ -50,6 +60,15 @@ public class HomeActivity extends AppCompatActivity {
         super.onDestroy();
         if(null != mAdView){
             mAdView.destroy();
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.bar_code_image_button) {
+            Toast.makeText(this, "Bar code", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "QR code", Toast.LENGTH_SHORT).show();
         }
     }
 }
